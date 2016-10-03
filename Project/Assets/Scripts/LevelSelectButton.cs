a0566be05678c90b2fce59220e0e0bd27cef701d
Pavel Barsukov <barsukovpt@outlook.com>
2016-10-04 01:32:27 +0300

    New Level Selector

---
+++ Project/Assets/Scripts/LevelSelectButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelSelectButton : MonoBehaviour {
+
+	public Text levelText;
+	public int unlocked;
+	public GameObject Star1;
+	public GameObject Star2;
+	public GameObject Star3;
+
+}
---
+++ Project/Assets/Scripts/NewLevelSelectManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public class NewLevelSelectManager : MonoBehaviour {
+
+	public GameObject levelButton;
+	public Transform spacer;
+	public List<Level> levelList;
+
+	[System.Serializable]
+	public class Level
+	{
+		public string levelText;
+		public int unLocked;
+		public bool isInteractable;
+
+		//public Button.ButtonClickedEvent onClickEvet;
+	}
+
+	void Start () 
+	{
+		FillList ();
+	}
+
+	void FillList()
+	{
+		foreach(var level in levelList)
+		{
+			GameObject newButton = Instantiate (levelButton) as GameObject;
+
+			LevelSelectButton button = newButton.GetComponent<LevelSelectButton > ();
+
+			button.levelText.text = level.levelText;
+
+			if (PlayerPrefs.GetInt("Level" + button.levelText.text) == 1)
+			{
+				level.unLocked = 1;
+				level.isInteractable = true;
+			}
+
+			button.unlocked = level.unLocked;
+			button.GetComponent<Button> ().interactable = level.isInteractable;
+			button.GetComponent<Button> ().onClick.AddListener (() => LoadLevels ("Level_" + button.levelText.text));
+
+			newButton.transform.SetParent (spacer);
+		}
+		SaveAll ();
+	}
+
+	void SaveAll()
+	{
+//		if (PlayerPrefs.HasKey ("Level1")) {
+//			return;
+//		} 
+//		else 
+//		{
+			GameObject[] allButtons = GameObject.FindGameObjectsWithTag ("LevelButton");
+			foreach (GameObject buttons in allButtons) 
+			{
+				LevelSelectButton button = buttons.GetComponent<LevelSelectButton> ();
+				PlayerPrefs.SetInt ("Level" + button.levelText.text, button.unlocked);
+			}
+		//}
+	}
+
+	void LoadLevels(string value)
+	{
+		Application.LoadLevel (value);
+	}
+
+	void DeleteAll()
+	{
+		PlayerPrefs.DeleteAll (); 
+	}
+}
